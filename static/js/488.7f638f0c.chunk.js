22746ff6fafdf8f340bc84eed670033f2538f36b
AmalAlalawi-dev <amal.alelawi@gmail.com>
2026-02-07 19:20:28 +0300

    Updates

---
+++ static/js/488.7f638f0c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgym_admin_dashboard=self.webpackChunkgym_admin_dashboard||[]).push([[488],{488(a,e,s){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.7f638f0c.chunk.js.map
